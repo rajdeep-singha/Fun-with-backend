46d3e21997af7705f416500cf12ca593f4fd2970
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-03-15 13:42:23 +0530

    fixing submodule

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios, { Axios, AxiosError } from "axios";
+import { useEffect } from "react";
+import Home from "./Component/home";
+import Contact from "./Component/contact";
+import About from "./Component/About";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import NavBar from "./Component/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
